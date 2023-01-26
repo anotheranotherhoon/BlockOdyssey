bafab0ed446da791db4e94f6e0e3477e13ddfc87
LeeChangHoon <changhoon1030@gmail.com>
2023-01-26 23:48:49 +0900

    ⭐ feat : react-query 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps<{ dehydratedState: DehydratedState }>) {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
