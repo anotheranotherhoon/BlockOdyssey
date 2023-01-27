727fd3e06059419f8dff56bad5314632968dd1ad
LeeChangHoon <changhoon1030@gmail.com>
2023-01-27 16:46:18 +0900

    ⭐ feat : url에서 쿼리 데이터 가져오는 함수

---
+++ src/utils/RouterInfo.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router"
+
+export const RouterInfo = () => {
+  const router = useRouter()
+  let filter = 'default'
+  let q = 'default'
+  router.query.filter ? filter = router.query.filter as string : filter
+  router.query.q ? q = router.query.q as string : q
+  return {
+    filter, q
+  }
+}
