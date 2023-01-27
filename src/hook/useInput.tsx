0d9eacb4f620513c3c938e5e910e73628381b433
LeeChangHoon <changhoon1030@gmail.com>
2023-01-28 03:23:59 +0900

    ⭐ feat : useInput 커스텀 훅 추가

---
+++ src/hook/useInput.tsx
@@ -0,0 +1,16 @@
+import React, {useState } from "react"
+import {RouterInfo} from '../utils/RouterInfo'
+
+export const useInput = () => {
+  const {q} = RouterInfo()
+  const [value, setValue] = useState(
+    q==='default' ? '' : q
+  )
+  const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+  return{
+    value,
+    handleInputChange
+  }
+}
