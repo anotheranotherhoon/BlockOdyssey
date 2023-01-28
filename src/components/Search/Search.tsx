e6581e96c3ab9c1d682e3173f9794d376eb6f0dc
LeeChangHoon <changhoon1030@gmail.com>
2023-01-28 18:04:44 +0900

    🎨 ui : 검색바 UI 작업

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -7,22 +7,22 @@ import styles from './Search.module.scss'
 import { FILTER, LIMIT } from '../../utils/constantValue'
 
 const Search = () => {
-  const {value, handleInputChange} = useInput()
-  const {router, filter, q, limit, page, selectedFilterName} = RouterInfo()
-  const { 
-    isDropDownShow: isFilterDropDownShow, 
-    selectedName: filterSelectedName, 
-    selectedStatus: filterSelectedStatus, 
-    handleDropDown: filterHandleDropDown, 
-    handleCurrentIndex: filterHandleCurrentIndex } = useDropDown(selectedFilterName, filter)
-  const { 
-    isDropDownShow: isLimitDropDownShow, 
-    selectedName: limitSelectedName, 
-    selectedStatus: limitSelectedStatus, 
-    handleDropDown: limitHandleDropDown, 
-    handleCurrentIndex: limitHandleCurrentIndex } = useDropDown(String(limit), String(limit))
+  const { value, handleInputChange } = useInput()
+  const { router, filter, q, limit, page, selectedFilterName } = RouterInfo()
+  const {
+    isDropDownShow: isFilterDropDownShow,
+    selectedName: filterSelectedName,
+    selectedStatus: filterSelectedStatus,
+    handleDropDown: filterHandleDropDown,
+    handleCurrentIndex: filterHandleCurrentIndex } = useDropDown(selectedFilterName, filter, false)
+  const {
+    isDropDownShow: isLimitDropDownShow,
+    selectedName: limitSelectedName,
+    selectedStatus: limitSelectedStatus,
+    handleDropDown: limitHandleDropDown,
+    handleCurrentIndex: limitHandleCurrentIndex } = useDropDown(String(limit), String(limit), true)
 
-  const handleSearch = (e : React.FormEvent<HTMLFormElement>) => {
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     router.push(`/?filter=${filterSelectedStatus}&q=${value}&page=${1}&limit=${limitSelectedStatus}`)
   }
@@ -32,10 +32,12 @@ const Search = () => {
       <div className={styles.bold} id={styles.split}>상품 검색</div>
       <section>
         <div className={styles.bold}>검색</div>
+        <div className={styles.flex_container}>
+          <DropDown isDropDownShow={isFilterDropDownShow} selectedName={filterSelectedName} selectedStatus={filterSelectedStatus} handleDropDown={filterHandleDropDown} handleCurrentIndex={filterHandleCurrentIndex} list={FILTER} />
+          <DropDown isDropDownShow={isLimitDropDownShow} selectedName={limitSelectedName} selectedStatus={limitSelectedStatus} handleDropDown={limitHandleDropDown} handleCurrentIndex={limitHandleCurrentIndex} list={LIMIT} />
+        </div>
         <form onSubmit={handleSearch}>
-          <DropDown isDropDownShow={isFilterDropDownShow} selectedName={filterSelectedName} selectedStatus={filterSelectedStatus} handleDropDown={filterHandleDropDown} handleCurrentIndex={filterHandleCurrentIndex} list={FILTER}/>
-          <DropDown isDropDownShow={isLimitDropDownShow} selectedName={limitSelectedName} selectedStatus={limitSelectedStatus} handleDropDown={limitHandleDropDown} handleCurrentIndex={limitHandleCurrentIndex} list={LIMIT}/>
-          <label htmlFor='q'/><input id='q' onChange={(e)=>handleInputChange(e)} value={value}/>
+          <label htmlFor='q' /><input id='q' onChange={(e) => handleInputChange(e)} value={value} />
           <button>조회</button>
         </form>
       </section>
