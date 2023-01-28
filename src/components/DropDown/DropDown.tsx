9cf4f36a25224483668f0881a1807581c416b60e
LeeChangHoon <changhoon1030@gmail.com>
2023-01-28 18:04:03 +0900

    🎨 ui : DropDown UI 작업

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,35 +1,36 @@
 import React from "react";
-import type {DropDownType} from '../../types/interfaces'
+import styles from './DropDown.module.scss'
+import type { DropDownType } from '../../types/interfaces'
 
 interface DropDownProps {
-  isDropDownShow : boolean;
-  selectedName : string | null;
-  selectedStatus :string | null;
-  handleDropDown : ()=>void;
-  handleCurrentIndex: (e :React.MouseEvent<HTMLButtonElement, MouseEvent>)=>void;
-  list : DropDownType[]
+  isDropDownShow: boolean;
+  selectedName: string | null;
+  selectedStatus: string | null;
+  handleDropDown: () => void;
+  handleCurrentIndex: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  list: DropDownType[]
 }
 
-const DropDown = ({isDropDownShow, selectedName, handleDropDown, handleCurrentIndex, list} : DropDownProps) => {
-  return(
-    <div>
-    <button type='button' onClick={handleDropDown}>
-      <span>{selectedName}</span>
-    </button>
-    {isDropDownShow && (
-      <ul>
-        {
-          list.map((filter)=> (
-            <li key={filter.id}>
-              <button type="button" onClick={(e)=>handleCurrentIndex(e)} value={filter.status}>
-                {filter.name}
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    )}
-  </div>
+const DropDown = ({ isDropDownShow, selectedName, handleDropDown, handleCurrentIndex, list }: DropDownProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <button type='button' className={styles.dropdown} onClick={handleDropDown}>
+        <span>{selectedName}</span>
+      </button>
+      {isDropDownShow && (
+        <ul className={styles.styleul}>
+          {
+            list.map((filter) => (
+              <li key={filter.id}>
+                <button type="button" onClick={(e) => handleCurrentIndex(e)} value={filter.status} className={styles.dropdown}>
+                  {filter.name}
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      )}
+    </div>
   )
 }
 
